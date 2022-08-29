390d622d067c5af16261c77d18da4d864cfc68f4
Gr-monaco <gr-monaco@hotmail.com>
2022-08-28 23:54:46 -0300

    Adiciona modelo de usuario

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        unique: true,
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    },
+    token: {
+        type: String
+    }
+})
