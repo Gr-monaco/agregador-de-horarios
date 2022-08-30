260fd42911ecd03ccbac73e689010cc1d895eb4d
Gr-monaco <gr-monaco@hotmail.com>
2022-08-30 20:01:08 -0300

    Adiciona logger básico

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,15 @@ const db = require('./database.js');
 const bp = require('body-parser');
 const app = express();
 const port = 3000
-
+const logger = require('./middleware/logger.js')
 //ROTAS
 const routesReuniao = require('./routes/reunioes.js');
 const routesUser = require('./routes/user.js')
 
 app.use(bp.json());
 app.use(bp.urlencoded({extended:true}));
+app.use(logger);
+
 app.use('/reuniao', routesReuniao)
 app.use('/user', routesUser)
 app.listen(port, () => console.log(`App listening on port ${port}.`));
