0d71f964056d3c2570f0c73745c883fe0f952794
Andr33Fel11p33 <andregalvao.06@outlook.com>
2022-09-18 20:11:22 -0300

    criando componente login

---
+++ Components/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, TextInput } from 'react-native';
+
+function Login(){
+
+return (
+    <View>
+ <TextInput></TextInput>
+    </View>
+)
+
+}
+export default Login
