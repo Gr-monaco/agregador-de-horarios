010bb79c58b5f45f5acc0ae98a2ad5fb6e9e5c7c
Gr-monaco <gr-monaco@hotmail.com>
2022-09-20 21:26:45 -0300

    Adiciona teste de autenticação

--- app/Components/login/login.js
+++ app/Components/login/login.js
@@ -3,38 +3,54 @@ import { View, TextInput, StyleSheet, Button, Text } from 'react-native';
 import { Formik } from 'formik';
 import { API_URL } from '@env';
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 
 function Login(){
-return (
-  <Formik
-    initialValues={{}}
-    onSubmit={values => {
-      console.log(API_URL + 'user/login');
-      axios.post(API_URL + 'user/login', values)
-      .then(res => console.log(res.body)).catch(err => console.log(err.response.data));
-    }}
-  >
-    {({ handleChange, handleBlur, handleSubmit, values }) => (
-      <View>
-        <Text> Usuario </Text>
-        <TextInput
-          onChangeText={handleChange('email')}
-          onBlur={handleBlur('email')}
-          value={values.email}
-          style={styles.input}/>
-        <Text> Senha </Text>
-        <TextInput
-          onChangeText={handleChange('senha')}
-          onBlur={handleBlur('senha')}
-          value={values.senha}
-          style={styles.input}
-          secureTextEntry={true}></TextInput>
-        <Button onPress={handleSubmit} title="Login"></Button>
-      </View>
-    )}
-  </Formik>
-    );
-}
+  async function testeDeAuth(){
+    const credentials = await SecureStore.getItemAsync('token');
+    console.log(credentials);
+    axios.post(API_URL + 'user/authTest', { token: credentials })
+    .then(res => console.log(res.data));
+  };
+
+  return (
+    <Formik
+      initialValues={{}}
+      onSubmit={values => {
+        console.log(API_URL + 'user/login');
+        axios.post(API_URL + 'user/login', values)
+        .then(async res => {
+          const token = res.data.token;
+          const email = res.data.email;
+          console.log(token);
+          console.log(email);
+          await SecureStore.setItemAsync('token', token);
+          console.log('Usuario Salvo');
+        }).catch(err => console.log(err.response.data));
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <Text> Usuario </Text>
+          <TextInput
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+            style={styles.input}/>
+          <Text> Senha </Text>
+          <TextInput
+            onChangeText={handleChange('senha')}
+            onBlur={handleBlur('senha')}
+            value={values.senha}
+            style={styles.input}
+            secureTextEntry={true}></TextInput>
+          <Button onPress={handleSubmit} title="Login"></Button>
+          <Button onPress={testeDeAuth} title="Teste de auth"></Button>
+        </View>
+      )}
+    </Formik>
+      );
+  }
 
 const styles = StyleSheet.create({
     input: {
