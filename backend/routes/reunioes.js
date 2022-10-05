b43f1ea30d7de0c95b8e4640fc16beb5792576d5
Gr-monaco <gr-monaco@hotmail.com>
2022-10-04 22:48:57 -0300

    Modificar para facilitar alteracoes futuras

--- backend/routes/reunioes.js
+++ backend/routes/reunioes.js
@@ -15,7 +15,22 @@ router.post('/adicionaHorario', auth, async (req, res) => {
     const update = {$push: { horariosDisponiveis: [{variosParticipantes: true, local: 'urlDoZoom', horarios: {horarioInicio: Date.now(), horarioFim: Date.now()}}]}}
     const options = { upsert: true, new: true,runValidators: true ,setDefaultsOnInsert: true };
 
+    const docReuniaoVerHorario = await Reuniao.findOne(query);
+    console.log(docReuniaoVerHorario);
+    if (docReuniaoVerHorario == null){
+        console.log('Doc não existe.');
+        const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
+        res.status(200).json({message: 'Doc criado.', docReuniao}).send();
+        return;
+    }
+
+    //aqui rodar validacoes de horario
     try{
+        console.elog
+        docReuniaoVerHorario.horariosDisponiveis.forEach((e, i, a) => {
+            //validacao de conflito de horario
+            console.log(e.horarios)
+        });
         const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
         res.status(200).json({message: 'Verifique console.', docReuniao}).send();
     }catch(e){
