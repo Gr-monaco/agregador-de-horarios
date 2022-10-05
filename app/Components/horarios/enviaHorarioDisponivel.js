c4f1c1984f9436c13ce30bd8d8def900f973256f
Gr-monaco <gr-monaco@hotmail.com>
2022-10-05 11:34:11 -0300

    Adicionar componente de enviar horario
    -- Nao esta implementado

---
+++ app/Components/horarios/enviaHorarioDisponivel.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, StyleSheet, Button, Text } from 'react-native';
+import Login from '../login/login';
+
+function EnviaHorarioDisponivel(){
+    return(
+        <Text>Oi</Text>
+    );
+}
+
+export default EnviaHorarioDisponivel;
