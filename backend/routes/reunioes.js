5f8325065ef2b3af137cfc9c21cceb5762a66080
Gr-monaco <gr-monaco@hotmail.com>
2022-11-03 00:12:41 -0300

    Salvar horario corretamente

--- backend/routes/reunioes.js
+++ backend/routes/reunioes.js
@@ -5,40 +5,61 @@ const auth = require('../middleware/auth.js');
 const router = express.Router();
 
 router.post('/adicionaHorario', auth, async (req, res) => {
+  console.log(req.body);
+  const userId = res.locals.user.user_id;
+  const userEmail = res.locals.user.email;
+  console.log(`Id do Usuario : ${userId}`);
+  console.log(`Email do Usuario : ${userEmail}`);
 
-    console.log(res.body);
-    const userId = res.locals.user.user_id;
-    const userEmail = res.locals.user.email;
-    console.log(`Id do Usuario : ${userId}`);
-    console.log(`Email do Usuario : ${userEmail}`);
+  const diaDaReuniao = new Date(req.body.dia);
+  const diaString = diaDaReuniao.getDate() + '/' +  diaDaReuniao.getMonth() + '/' + diaDaReuniao.getFullYear()
+  console.log(`DIA DA REUNIAO: ${diaString}`);
 
-    const query = {userId: userId};
-    const update = {$push: { horariosDisponiveis: [{variosParticipantes: true, local: 'urlDoZoom', horarios: {horarioInicio: Date.now(), horarioFim: Date.now()}}]}}
-    const options = { upsert: true, new: true,runValidators: true ,setDefaultsOnInsert: true };
+  const query = { userId: userId };
+  const update = {
+    $push: {
+      horariosDisponiveis: [
+        {
+          variosParticipantes: true,
+          local: 'urlDoZoom',
+          dia: diaString,
+          horarios: {
+            horarioInicio: req.body.horarioInicial,
+            horarioFim: req.body.horarioTermino,
+          },
+        },
+      ],
+    },
+  };
+  const options = {
+    upsert: true,
+    new: true,
+    runValidators: true,
+    setDefaultsOnInsert: true,
+  };
 
-    const docReuniaoVerHorario = await Reuniao.findOne(query);
-    console.log(docReuniaoVerHorario);
-    if (docReuniaoVerHorario == null){
-        console.log('Doc não existe.');
-        const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
-        res.status(200).json({message: 'Doc criado.', docReuniao}).send();
-        return;
-    }
+  const docReuniaoVerHorario = await Reuniao.findOne(query);
+  console.log(docReuniaoVerHorario);
+  if (docReuniaoVerHorario == null) {
+    console.log('Doc não existe.');
+    const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
+    res.status(200).json({ message: 'Doc criado.', docReuniao }).send();
+    return;
+  }
 
-    //aqui rodar validacoes de horario
-    try{
-        console.elog
-        docReuniaoVerHorario.horariosDisponiveis.forEach((e, i, a) => {
-            //validacao de conflito de horario
-            console.log(e.horarios)
-        });
-        const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
-        res.status(200).json({message: 'Verifique console.', docReuniao}).send();
-    }catch(e){
-        // TODO: retornar erro sem stacktrace
-        res.status(500).json({message: e}).send();
-    }
-    return
-})
+  //aqui rodar validacoes de horario
+  try {
+    docReuniaoVerHorario.horariosDisponiveis.forEach((e, i, a) => {
+      //validacao de conflito de horario
+      console.log(e.horarios);
+    });
+    const docReuniao = await Reuniao.findOneAndUpdate(query, update, options);
+    res.status(200).json({ message: 'Verifique console.', docReuniao }).send();
+  } catch (e) {
+    // TODO: retornar erro sem stacktrace
+    res.status(500).json({ message: e }).send();
+  }
+  return;
+});
 
 module.exports = router;
